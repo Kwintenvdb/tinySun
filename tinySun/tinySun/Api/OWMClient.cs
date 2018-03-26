fb03fcffd491194996b814900ecf9a745e985cbd
Kwinten Van den Berghe <kwintenvdberghe@gmail.com>
2018-03-26 21:06:23 +0200

    Add project files.

---
+++ tinySun/tinySun/Api/OWMClient.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace tinySun.Api
+{
+	// Open Weather Map client
+	public class OWMClient
+	{
+		private const string BASE_URI = "http://api.openweathermap.org/data/2.5/";
+		private const string APP_ID = "&APPID=a5f99fb3c430c84a4ff4e637291a81af";
+
+		public async Task<T> SendRequest<T>(string request)
+		{
+			try
+			{
+				using (var httpClient = new HttpClient())
+				{
+					var response = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, $"{BASE_URI}{request}{APP_ID}"));
+					if (response.IsSuccessStatusCode)
+					{
+						string content = await response.Content.ReadAsStringAsync();
+						T obj = JsonConvert.DeserializeObject<T>(content);
+						return obj;
+					}
+				}
+			}
+			catch (System.Exception e)
+			{
+
+			}
+
+			return default(T);
+		}
+	}
+}
---
+++ tinySun/tinySun/DataProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tinySun.Api;
+
+namespace tinySun
+{
+	public class DataProvider
+	{
+		private OWMClient _client = new OWMClient();
+		
+		public async void GetCurrentWeather()
+		{
+			var weather = await _client.SendRequest<object>("weather?q=London");
+		}
+	}
+}
